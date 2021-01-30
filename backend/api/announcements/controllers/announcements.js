8ec61884e9b5cedd15f8903a84763109a6c11435
pxdc-studio <nam.nguyen@paradoxcapsule.studio>
2021-01-30 09:49:27 +0700

    move to full orm based subcription

--- backend/api/announcements/controllers/announcements.js
+++ backend/api/announcements/controllers/announcements.js
@@ -6,34 +6,226 @@
  */
 
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const _ = require("lodash");
+
+let { createTags, createResources } = strapi.config.functions["common"];
 
 module.exports = {
   async find(ctx) {
-    let raw = await strapi
-      .query("announcements")
-      .find({ _limit: 20, _sort: "published_at:desc" });
-    return raw.map((item) =>
-      sanitizeEntity(item, { model: strapi.models.announcements })
-    );
+    try {
+      let user = ctx.state.user;
+
+      let fromUser = 0;
+
+      let resSCHEMA = {
+        data: [],
+        page: 0,
+        pageSize: 0,
+        rowCount: 0,
+        pageCount: 0,
+      };
+
+      let sub = await strapi.query("subscriptions").findOne({ "user.id": 1 });
+
+      if (!sub) {
+        return resSCHEMA;
+      }
+
+      let tagsFromSynonyms = await strapi
+        .query("synonyms")
+        .find({ slug_in: sub.synonyms.map((o) => o.synonym.slug) });
+
+      // prepare tags into common standards
+      // we can improve synonym to ignore author here
+      let tags = sub.tags.concat(
+        _.flatten(
+          tagsFromSynonyms.map((synonym) =>
+            synonym.tags.map((tag) => ({ tag: tag, exclude_authors: [] }))
+          )
+        )
+      );
+
+      let raw = await strapi
+        .query("announcements")
+        .model.query(function (q) {
+          // left join is currently the only solution to produce complex query using knexjs,bookshelfjs type of ORM module
+          q.leftJoin(
+            "announcements__tags",
+            "announcements.id",
+            "announcements__tags.announcement_id"
+          );
+
+          q.leftJoin("tags", "announcements__tags.tag_id", "tags.id");
+
+          q.where("announcements.author", "!=", fromUser);
+
+          tags.forEach((node) => {
+            q.orWhere(function () {
+              let self = this.where("announcements__tags.tag_id", node.tag.id);
+
+              if (node.exclude_authors && node.exclude_authors.length > 0) {
+                self.andWhere(
+                  "announcements.author",
+                  "NOT IN",
+                  node.exclude_authors.map((author) => author.id)
+                );
+              }
+            });
+          });
+
+          sub.authors.forEach((node) => {
+            q.orWhere(function () {
+              let self = this.where("announcements.author", node.id);
+
+              if (node.exclude_tags && node.exclude_tags.length > 0) {
+                self.andWhere(
+                  "tags.id",
+                  "NOT IN",
+                  node.exclude_tags.map((o) => o.id)
+                );
+              }
+            });
+          });
+
+          q.orderBy("postdate", "desc");
+        })
+        .fetchPage({
+          pageSize: 20,
+          page: 1,
+          debug: true,
+        });
+
+      resSCHEMA = {
+        data: raw.map((item) =>
+          sanitizeEntity(item, { model: strapi.models.announcements })
+        ),
+        ...raw.pagination,
+      };
+
+      return resSCHEMA;
+    } catch (e) {
+      console.error("get-sub-error", e);
+      return;
+    }
   },
-  async findByUser(ctx) {
-    let raw = await strapi
-      .query("announcements")
-      .find({ _limit: 20, _sort: "published_at:desc", author: ctx.params.id });
-    return raw.map((item) =>
+  async findByOwner(ctx) {
+    let user = ctx.state.user;
+    let authorId = user;
+
+    let { limit = 20, start = 0 } = ctx.query;
+
+    let model = strapi.query("announcements");
+
+    let raw = model.find({
+      _start: start,
+      _limit: limit,
+      _sort: "published_at:desc",
+      author: 1,
+    });
+
+    let count = model.count({ author: 1 });
+
+    [raw, count] = await Promise.all([raw, count]);
+
+    let result = raw.map((item) =>
       sanitizeEntity(item, { model: strapi.models.announcements })
     );
+
+    return {
+      start,
+      limit,
+      total: count,
+      data: result,
+    };
   },
   async create(ctx) {
-    let { title, message, tags, resources } = ctx.request.body;
-    console.log({
+    let user = ctx.state.user;
+    let authorId = 1;
+
+    let {
       title,
       message,
-      tags,
-      resources,
-    });
-    return { status: 200 };
+      tags: input_tags,
+      resources: input_resources,
+      synonyms,
+    } = ctx.request.body;
 
-    // strapi.query("announcements")
+    try {
+      const [_tags, _resouces] = await Promise.all([
+        createTags(input_tags, authorId),
+        createResources(input_resources, authorId),
+      ]);
+
+      await strapi.query("announcements").create({
+        title: title || "default",
+        message: message || "default",
+        tags: _tags,
+        resources: _resouces,
+        synonyms: synonyms,
+        postdate: new Date(),
+        author: 1,
+      });
+
+      return { status: 200 };
+    } catch (e) {
+      return { status: 400 };
+    }
+  },
+  async update(ctx) {
+    let user = ctx.state.user;
+    let authorId = 1;
+    let { id } = ctx.params;
+
+    if (!id) {
+      return { status: 400 };
+    }
+
+    let isYourPost = await strapi
+      .query("announcements")
+      .find({ author: authorId, id: id });
+
+    if (!isYourPost) {
+      return { status: 400 };
+    }
+
+    let {
+      title,
+      message,
+      tags: input_tags,
+      resources: input_resources,
+      synonyms,
+    } = ctx.request.body;
+
+    try {
+      let [_tags, _resouces] = await Promise.all([
+        createTags(input_tags, authorId),
+        createResources(input_resources, authorId),
+      ]);
+
+      await strapi.query("announcements").update(
+        { id: id },
+        {
+          ...(title && { title }),
+          ...(message && { message }),
+          tags: _tags,
+          resources: _resouces,
+          synonyms: synonyms,
+          author: 1,
+        }
+      );
+
+      return { status: 200 };
+    } catch (e) {
+      return { status: 400 };
+    }
+  },
+  async delete(ctx) {
+    let { id } = ctx.params;
+
+    if (!id) {
+      return { status: 400 };
+    }
+
+    return await strapi.query("announcements").delete({ id: id });
   },
 };
---
+++ backend/config/functions/common.js
@@ -0,0 +1,167 @@
+exports.createTags = async function (input_tags, authorId) {
+  /**
+   * Tags Cleanup
+   * 1. Isolate object type tag for insertion
+   * 2. Check if insertion tag existed
+   * 3. Insert new tags
+   */
+
+  if (!input_tags || (input_tags && input_tags.length < 1)) {
+    return;
+  }
+
+  let tags = [];
+  let tags_to_insert = [];
+
+  for (var i = 0; i < input_tags.length; i++) {
+    let item = input_tags[i];
+    if (typeof item == "object") {
+      tags_to_insert.push(item);
+    } else {
+      tags.push(item);
+    }
+  }
+
+  async function create() {
+    if (tags_to_insert.length > 0) {
+      const exist_tags = await strapi.query("tags").find({
+        slug_in: tags_to_insert.map((tag) => tag.slug),
+      });
+
+      //Insert to tags list if tags already exist
+      tags = tags.concat(exist_tags.map((e) => e.id));
+
+      //Remove existing tags from insert list
+      tags_to_insert = tags_to_insert.filter(
+        (item) => !exist_tags.map((e) => e.slug).includes(item.slug)
+      );
+
+      //Create tag
+      tags_to_insert.map(async (tag) => {
+        return await strapi
+          .query("tags")
+          .create({ slug: tag.slug, author: authorId });
+      });
+
+      let inserted_tags = await Promise.all(tags_to_insert);
+
+      //Retrieve new id for tags (this is really bad, but this api return no ID when insert)
+      const new_tags = await strapi.query("tags").find({
+        slug_in: inserted_tags.map((tag) => tag.slug),
+      });
+
+      tags = tags.concat(new_tags.map((e) => e.id));
+    }
+    return tags;
+  }
+
+  await create();
+
+  return tags;
+};
+
+exports.createResources = async function (input_resources, authorId) {
+  if (!input_resources || (input_resources && input_resources.length < 1)) {
+    return;
+  }
+
+  let resources = [];
+  let resources_to_insert = [];
+
+  for (var i = 0; i < input_resources.length; i++) {
+    let item = input_resources[i];
+    if (typeof item == "object") {
+      resources_to_insert.push(item);
+    } else {
+      resources.push(item);
+    }
+  }
+
+  async function create() {
+    if (resources_to_insert.length > 0) {
+      const exist_resources = await strapi.query("resources").find({
+        url_in: resources_to_insert.map((r) => r.url),
+      });
+
+      resources = resources.concat(exist_resources.map((e) => e.id));
+
+      resources_to_insert = resources_to_insert.filter(
+        (item) => !exist_resources.map((e) => e.url).includes(item.url)
+      );
+
+      resources_to_insert.map(async (r) => {
+        return await strapi
+          .query("resources")
+          .create({ name: r.name, url: r.url, author: authorId });
+      });
+
+      let inserted_tags = await Promise.all(resources_to_insert);
+
+      //Retrieve new id for tags (this is really bad, but this api return no ID when insert)
+      const new_resources = await strapi.query("resources").find({
+        url_in: inserted_tags.map((r) => r.url),
+      });
+
+      resources = resources.concat(new_resources.map((e) => e.id));
+    }
+
+    return resources;
+  }
+
+  await create();
+
+  return resources;
+};
+
+exports.createSynonyms = async function (input_synonyms, authorId) {
+  if (!input_synonyms || (input_synonyms && input_synonyms.length < 1)) {
+    return;
+  }
+
+  let synonyms = [];
+  let synonyms_to_insert = [];
+
+  for (var i = 0; i < input_synonyms.length; i++) {
+    let item = input_synonyms[i];
+    if (typeof item == "object") {
+      synonyms_to_insert.push(item);
+    } else {
+      synonyms.push(item);
+    }
+  }
+
+  async function create() {
+    if (synonyms_to_insert.length > 0) {
+      const exist_synonyms = await strapi.query("synonyms").find({
+        slug_in: synonyms_to_insert.map((r) => r.slug),
+      });
+
+      synonyms = synonyms.concat(exist_synonyms.map((e) => e.id));
+
+      synonyms_to_insert = synonyms_to_insert.filter(
+        (item) => !exist_synonyms.map((e) => e.slug).includes(item.slug)
+      );
+
+      synonyms_to_insert.map(async (r) => {
+        return await strapi
+          .query("synonyms")
+          .create({ slug: r.slug, author: authorId });
+      });
+
+      let inserted_tags = await Promise.all(synonyms_to_insert);
+
+      //Retrieve new id for tags (this is really bad, but this api return no ID when insert)
+      const new_synonyms = await strapi.query("synonyms").find({
+        slug_in: inserted_tags.map((r) => r.slug),
+      });
+
+      synonyms = synonyms.concat(new_synonyms.map((e) => e.id));
+    }
+
+    return synonyms;
+  }
+
+  await create();
+
+  return synonyms;
+};
